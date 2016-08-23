599f444c9a1aeb2287c142ed49646328486c1438
赵巍 <huaguzheng2007@126.com>
2016-08-23 18:38:37 +0800

    no message

--- main.js
+++ main.js
@@ -5,31 +5,14 @@ module.exports = function (context) {
   return context + `
     declare var module;
     declare var require;
-    var app = require('laya').default;
+    var _laya = require('laya').default;
     if (module.hot) {
         module.hot.accept();
         module.hot.dispose(function(data) {
-            app.clearCptAllData('${cptName}');
-            var curSence = app.getCurSence();
-            var vm = app.componentDataMap;
-            var cl = new Map();
-            var keys = vm.keys();
-            var next;
-            while (next = keys.next(), !next.done) {
-                var ineMap = vm.get(next.value);
-                var ineRet = new Map();
-                var ineKeys = ineMap.keys();
-                var ineNext;
-                var i = 0;
-                while (ineNext = ineKeys.next(), !ineNext.done) {
-                    ineRet.set(i++, ineMap.get(ineNext.value));
-                }
-                cl.set(next.value, ineRet);
-            }
-            setTimeout(() => {
-                app.destoryCptForSence(curSence);
-                app.buildSence(curSence, app.sence.get(curSence), cl, ['${cptName}']);
-            }, 32);
+            _laya.cancelComponent('${cptName}');
+            setTimeout(function() {
+                _laya.rebuildSence();
+            }, 48);
         });
     }
   `
