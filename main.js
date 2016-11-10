4416e9695546a7c8c57e23e4394b09f1bd0e0133
赵巍 <huaguzheng2007@126.com>
2016-11-10 14:14:14 +0800

    no message

--- main.js
+++ main.js
@@ -1,19 +1,37 @@
 var path = require('path');
 
 module.exports = function (context) {
-  var cptName = path.basename(this.resource, '.ts').replace('_lc', '');
-  return context + `
-    declare var module;
-    declare var require;
-    var _laya = require('laya').default;
-    if (module.hot) {
-        module.hot.accept();
-        module.hot.dispose(function(data) {
-            _laya.cancelComponent('${cptName}');
-            setTimeout(function() {
-                _laya.rebuildSence();
-            }, 48);
-        });
-    }
-  `
+  var cptName = path.basename(this.resource, '.ts');
+  var component = /@component/.test(context);
+  var sence = /@sence/.test(context);
+  if (component === true || sence === true) {
+      return context + `
+        declare var module;
+        declare var require;
+        var _laya = require('laya').default;
+        if (module.hot) {
+            module.hot.accept();
+            module.hot.dispose(function(data) {
+                _laya.cancelComponent('${cptName}');
+                setTimeout(function() {
+                    _laya.rebuildSence();
+                }, 48);
+            });
+        }
+    `
+  } else {
+      return context + `
+        declare var module;
+        declare var require;
+        var _laya = require('laya').default;
+        if (module.hot) {
+            module.hot.accept();
+            module.hot.dispose(function(data) {
+                setTimeout(function() {
+                    _laya.rebuildSence();
+                }, 48);
+            });
+        }
+      `
+  }
 }
